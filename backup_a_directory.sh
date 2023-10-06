a074c4cb46f1447486a1f4841495e9449767f395
Kamil Kowol <63851737+kkowol@users.noreply.github.com>
2023-10-06 23:16:52 +0200

    Update backup_a_directory.sh

--- backup_a_directory.sh
+++ backup_a_directory.sh
@@ -3,5 +3,5 @@ TIME=`date +%d_%m_%y`                   # This command will read the date.
 FILENAME=NAME_$TIME.tar.gz    		# The filename including the date.
 SRCDIR=/PATH/TO/SOURCE       		# Source backup folder.
 DESDIR=/PATH/TO/DESTINATION  		# Destination of backup file.
-tar --exclude='/EXCLUDE/SOME/FILESorDATAinDOURCE' -cpzf $DESDIR/$FILENAME $SRCDIR
+tar --exclude='/EXCLUDE/SOME/FILESorDATAinSOURCE' -cpzf $DESDIR/$FILENAME $SRCDIR
 # example: tar --exclude='/PATH/TO/SOURCE/.git' -cpzf $DESDIR/$FILENAME $SRCDIR
