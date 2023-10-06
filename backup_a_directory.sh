9813b477c54507f77b934e4089f7f12bc304676c
Kamil <kowol@tutamail.com>
2023-10-06 23:15:28 +0200

    initial commit

---
+++ backup_a_directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+TIME=`date +%d_%m_%y`                   # This command will read the date.
+FILENAME=NAME_$TIME.tar.gz    		# The filename including the date.
+SRCDIR=/PATH/TO/SOURCE       		# Source backup folder.
+DESDIR=/PATH/TO/DESTINATION  		# Destination of backup file.
+tar --exclude='/EXCLUDE/SOME/FILESorDATAinDOURCE' -cpzf $DESDIR/$FILENAME $SRCDIR
+# example: tar --exclude='/PATH/TO/SOURCE/.git' -cpzf $DESDIR/$FILENAME $SRCDIR
